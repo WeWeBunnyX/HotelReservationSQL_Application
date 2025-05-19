0f518fc6f9035182faee252ab58fcafc9093b934
Hasan Zahid <hasanzafzal@gmail.com>
2025-05-19 12:27:58 +0500

    Fix UI properties

--- customers.cpp
+++ customers.cpp
@@ -6,6 +6,12 @@ CustomersModule::CustomersModule(QWidget *parent) :
     ui(new Ui::CustomersModule)
 {
     ui->setupUi(this);
+
+    ui->customersTable->setColumnWidth(0, 200);
+    ui->customersTable->setColumnWidth(1, 200);
+    ui->customersTable->setColumnWidth(2, 200);
+    ui->customersTable->setColumnWidth(3, 170);
+    ui->customersTable->setColumnWidth(4, 170);
 }
 
 CustomersModule::~CustomersModule()
