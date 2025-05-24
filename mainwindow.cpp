567694e69586dc9256c682923c81f76b376e65a2
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2025-05-24 19:34:06 +0500

    Welcome Screen UI changes
    - Add Qt and GPL v3 Logos

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,28 +1,42 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "dashboard.h"
+#include <QDesktopServices>
+#include <QUrl>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , dashboard(nullptr)
+    , customersModule(nullptr)
+    , roomsModule(nullptr)
+    , paymentsModule(nullptr)
+    , reportsModule(nullptr)
 {
     ui->setupUi(this);
-
+    connect(ui->welcomeLabel, &QLabel::linkActivated, this, &MainWindow::onLinkActivated);
 }
 
 MainWindow::~MainWindow()
 {
+    delete dashboard;
+    delete customersModule;
+    delete roomsModule;
+    delete paymentsModule;
+    delete reportsModule;
     delete ui;
 }
 
 void MainWindow::onLinkActivated(const QString &link)
 {
     if (link == "license") {
-        QDesktopServices::openUrl(QUrl("https://github.com/yourusername/HotelReservationSQL_Application/blob/main/LICENSE"));
+        QDesktopServices::openUrl(QUrl("https://github.com/WeWeBunnyX/HotelReservationSQL_Application/blob/master/LICENSE"));
     } else if (link == "github") {
-        QDesktopServices::openUrl(QUrl("https://github.com/yourusername/HotelReservationSQL_Application"));
+        QDesktopServices::openUrl(QUrl("https://github.com/WeWeBunnyX/HotelReservationSQL_Application"));
     } else if (link == "start") {
-        Dashboard *dashboard = new Dashboard(this);
+        if (!dashboard) {
+            dashboard = new Dashboard(this);
+        }
         dashboard->show();
         hide();
     }
