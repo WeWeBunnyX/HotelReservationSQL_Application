8787ff13044b85ae8b7bd29894224dac7247d8ee
Hasan Zahid <hasanzafzal@gmail.com>
2025-05-26 12:38:57 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,10 @@
 #include <QDebug>
 #include "database.h"
 #include "loginscreen.h"
-
 // #include "mainwindow.h"
 // #include "loginscreen.h"
 // #include "registerscreen.h"
-// #include "dashboard.h"
+#include "dashboard.h"
 // #include "reservations.h"
 
 int main(int argc, char *argv[])
